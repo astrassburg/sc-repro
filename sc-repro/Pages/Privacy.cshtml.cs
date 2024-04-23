acc2b622f6b42daa2d24026f1f3f704cbfb33b85
astrassburg <astrassburg@nas.edu>
2024-04-23 09:49:20 -0400

    Add project files.

---
+++ sc-repro/Pages/Privacy.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sc_repro.Pages {
+	public class PrivacyModel : PageModel {
+		private readonly ILogger<PrivacyModel> _logger;
+
+		public PrivacyModel(ILogger<PrivacyModel> logger) {
+			_logger = logger;
+		}
+
+		public void OnGet() {
+		}
+	}
+
+}
